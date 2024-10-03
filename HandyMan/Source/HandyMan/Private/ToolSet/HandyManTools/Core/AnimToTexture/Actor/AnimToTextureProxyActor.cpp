4e2a57ca4f51e84d6ed2296d267936a4f11c4d7e
Greggory Addison <mewe.interactive@gmail.com>
2024-10-03 12:26:08 -0500

    [Anim To Texture]
    - Laid foundation classes for a command that bakes animation out to textures.

---
+++ HandyMan/Source/HandyMan/Private/ToolSet/HandyManTools/Core/AnimToTexture/Actor/AnimToTextureProxyActor.cpp
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimToTextureProxyActor.h"
+
+
+// Sets default values
+AAnimToTextureProxyActor::AAnimToTextureProxyActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAnimToTextureProxyActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAnimToTextureProxyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ HandyMan/Source/HandyMan/Private/ToolSet/HandyManTools/Core/AnimToTexture/Tool/BakeAnimToTexture.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ToolSet/HandyManBaseClasses/HandyManSingleClickTool.h"
+#include "BakeAnimToTexture.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HANDYMAN_API UBakeAnimToTexture : public UHandyManSingleClickTool
+{
+	GENERATED_BODY()
+};
